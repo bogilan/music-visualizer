790056914fb733bde42fd447aa4e8a133db499cc
bogilan <bogdan.lakicevic@gmail.com>
2023-05-18 21:48:16 +0200

    Added colorful visualization and play/pause/stop buttons functionality.

--- script.js
+++ script.js
@@ -2,6 +2,14 @@
 const canvas = document.getElementById('visualizer');
 const ctx = canvas.getContext('2d');
 
+// Buttons
+const playBtn = document.querySelector('.play');
+const pauseBtn = document.querySelector('.pause');
+const stopBtn = document.querySelector('.stop');
+
+// Variable to track visualization state
+let isPaused = false;
+
 // Create an Audio element and set its src property to the URL of an audio file.
 const audio = new Audio();
 audio.src = 'inspire.mp3';
@@ -21,6 +29,12 @@ const dataArray = new Uint8Array(bufferLength);
 
 // Define a draw function that will be called repeatedly to update the visualizer.
 function draw() {
+    // Only draw if not paused
+    // If paused, visualization stops and freezes
+    if (isPaused) {
+      return;
+    } 
+
     // Schedule the next call to draw.
     requestAnimationFrame(draw);
 
@@ -36,15 +50,18 @@ function draw() {
     for (let i = 0; i < bufferLength; i++) {
       const barHeight = (dataArray[i] / 255) * canvas.height;
       // Set the fill style of the rectangle based on the current frequency value.
-      ctx.fillStyle = `rgb(${barHeight}, ${barHeight}, ${barHeight})`;
+      const hue = i / bufferLength * 360;
+      const color = `hsla(${hue}, 100%, 50%, 1)`;
+      ctx.fillStyle = color;
        // Draw the rectangle at the appropriate position and size on the canvas.
       ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
       x += barWidth;
     }
 }
 
-// Play the audio file when the user clicks on page.
-document.addEventListener('click', function() {
+// Play the audio file when the user clicks on PLAY button.
+playBtn.addEventListener('click', function() {
+    isPaused = false; // Resume visualization
     audio.play();
     // Music by: https://www.bensound.com/free-music-for-videos
     // License code: 9NF0OTIYZCXIRVJN
@@ -56,4 +73,16 @@ document.addEventListener('click', function() {
     draw();
 });
   
- 
+ // Pause the audio file and visualization when the user clicks on PAUSE button.
+ pauseBtn.addEventListener('click', function() {
+    isPaused = true; // Pause visualization
+    audio.pause();
+});
+
+// Stop the audio file and visualization when the user clicks on STOP button.
+stopBtn.addEventListener('click', function() {
+    isPaused = false; // Resume visualization
+    audio.currentTime = 0; // Reset audio playback to the beginning
+    audio.pause();
+    draw();
+});
